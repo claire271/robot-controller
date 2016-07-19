8f10be548d06aefee289a20af1fe680570bbb101
tigerh <tigerrun1998@gmail.com>
2016-07-19 17:43:47 +0000

    Fixed issue with byteDelimiter parser and also interpreted lidar bytes correctly.

--- server/index.js
+++ server/index.js
@@ -5,14 +5,41 @@ var io = require('socket.io')(http);
 var SerialPort = require('serialport');
 var ekf_slam = require('./ekf_slam').ekf_slam;
 
+//Only returns if the delimiter is found consecutively
+//Derived from the source of node-serialport
+function consecutiveByteDelimiter(delimiter) {
+  if (Object.prototype.toString.call(delimiter) !== '[object Array]') {
+    delimiter = [ delimiter ];
+  }
+  var buf = [];
+  return function(emitter, buffer) {
+    for (var i = 0; i < buffer.length; i++) {
+      buf[buf.length] = buffer[i];
+
+
+      var match = true;
+      for(var j = 0,k = buf.length - delimiter.length;j < delimiter.length;j++,k++) {
+        if(delimiter[j] !== buf[k]) {
+          match = false;
+          break;
+        }
+      }
+      if(match) {
+        emitter.emit('data', buf);
+        buf = [];
+      }
+    }
+  };
+}
+
 //Serial to/from the C & ASM PRU controller
 var serial = new SerialPort.SerialPort("./serial0", {
   baudRate: 115200,
-  parser: SerialPort.parsers.byteDelimiter([0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00])
+  parser: consecutiveByteDelimiter([0xFF,0xFF,0xFF,0xFF,0xFF,0xFF])
 });
 serial.on('data',function(data) {
-  var left_in = (data[3] << 24 | data[2] << 16 | data[1] << 8 | data[0]) & 0xFFFFFFFF;
-  var right_in = (data[9] << 24 | data[8] << 16 | data[7] << 8 | data[6]) & 0xFFFFFFFF;
+  var left_in = (data[4] << 24 | data[3] << 16 | data[2] << 8 | data[1]) & 0xFFFFFFFF;
+  var right_in = (data[10] << 24 | data[9] << 16 | data[8] << 8 | data[7]) & 0xFFFFFFFF;
 
   var encoders = {};
   encoders.left = left_in;
@@ -30,11 +57,10 @@ const IH = 480;
 //Initializing slam
 //var slam = new ekf_slam();
 
-/*
 //Serial to/from the LIDAR
 var serial2 = new SerialPort.SerialPort("/dev/ttyS2", {
   baudRate: 115200,
-  parser: SerialPort.parsers.byteDelimiter([0xFF])
+  parser: consecutiveByteDelimiter([0xFF,0xFF,0xFF])
 });
 serial2.on('data',function(data) {
   //Bail if incomplete frame
@@ -43,18 +69,22 @@ serial2.on('data',function(data) {
   var points = [];
   var npoints = 0;
   for(var i = 0;i < IW;i++) {
+    var i1 = Math.floor(i * 1.5) + 1;
+    var i2 = Math.floor(i / 2) * 3;
+
     //Bail if no point detected in this column
-    if(data[i] == 254) continue;
+    if(data[i1] == 254) continue;
+
+    var value = data[i1] + (((i % 2) ? data[i2] >> 4 : data[i2]) & 0x0F) / 16;
 
     points[npoints] = {};
-    points[npoints].y = VD * H * 1.0 / data[i];
+    points[npoints].y = VD * H * 1.0 / value;
     points[npoints].x = points[npoints].y * (i - IW/2) / VD;
     npoints++;
   }
 
   io.emit('lidar data',points);
 });
-*/
 
 //Web routing information
 app.get('/', function(req, res){
